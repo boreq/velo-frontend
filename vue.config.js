9b5cea841b715f606bf6f3de96163bd1bb63e70c
boreq <boreq@0x46.net>
2019-10-18 18:13:53 +0200

    Bump versions

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                data: `
+                prependData: `
                     @import "@/scss/variables.scss";
                 `
             }
