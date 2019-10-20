329ec2ebcca6f0e73bc55076f60b967028b30f38
boreq <boreq@0x46.net>
2019-10-21 01:13:33 +0200

    Add hours to the duration format

--- src/services/TextService.ts
+++ src/services/TextService.ts
@@ -1,9 +1,14 @@
 export class TextService {
 
     formatTime(numberOfSeconds: number): string {
-        const minutes = Math.floor(numberOfSeconds / 60);
+        const hours = Math.floor(numberOfSeconds / 3600);
+        const minutes = Math.floor((numberOfSeconds % 3600) / 60);
         const seconds = this.pad(Math.floor(numberOfSeconds % 60), 2);
-        return `${minutes}:${seconds}`;
+        if (hours) {
+            return `${hours}:${this.pad(minutes, 2)}:${this.pad(seconds, 2)}`;
+        } else {
+            return `${minutes}:${this.pad(seconds, 2)}`;
+        }
     }
 
     private pad(num: number, size: number): string {
