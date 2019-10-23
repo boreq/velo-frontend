fb8d13b79767aadea18d8f1bfa58eaa268d68815
boreq <boreq@0x46.net>
2019-10-24 01:52:11 +0200

    Show a spinner while converting thumbnails

--- src/components/Thumbnail.ts
+++ src/components/Thumbnail.ts
@@ -1,13 +1,25 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Ref, Vue } from 'vue-property-decorator';
 import { Album } from '@/dto/Album';
 import { ApiService } from '@/services/ApiService';
+import Spinner from '@/components/Spinner.vue';
 
 
-@Component
+@Component({
+    components: {
+        Spinner,
+    },
+})
 export default class Thumbnail extends Vue {
 
     @Prop()
     album: Album;
+
+    @Ref()
+    image: HTMLImageElement;
+
+    converting = false;
+
+    private timeoutId: number;
 
     private apiService = new ApiService();
 
@@ -16,6 +28,23 @@ export default class Thumbnail extends Vue {
             return this.apiService.thumbnailUrl(this.album);
         }
         return null;
+    }
+
+    onError(event: Event): void {
+        this.converting = true;
+        this.reload();
+
+    }
+
+    onLoad(event: Event): void {
+        this.converting = false;
+    }
+
+    private reload() {
+        if (this.timeoutId) {
+            window.clearTimeout(this.timeoutId);
+        }
+        this.timeoutId = window.setTimeout(() => this.image.src = this.thumbnailUrl, 5000);
     }
 
 }
--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -1,5 +1,10 @@
 <template>
-    <img v-if="album.thumbnail" :src="thumbnailUrl">
+    <div class="thumbnail" v-if="album.thumbnail">
+        <img :src="thumbnailUrl" @error="onError" @load="onLoad" ref="image">
+        <div class="spinner-container" v-if="converting" v-tooltip="'This thumbnail has not been converted yet.'">
+            <spinner></spinner>
+        </div>
+    </div>
 </template>
 <script lang="ts" src="./Thumbnail.ts"></script>
 <style scoped lang="scss" src="./Thumbnail.scss"></style>
