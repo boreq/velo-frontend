92be1d1ac80af5f364055c02652ad045e2108e49
boreq <boreq@0x46.net>
2019-11-04 23:21:30 +0100

    Settings design

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -8,6 +8,12 @@
             <th class="role">
                 Role
             </th>
+            <th class="last-seen">
+                Last Seen
+            </th>
+            <th class="actions">
+                Actions
+            </th>
             </thead>
             <tbody>
             <tr class="user" v-for="user in users">
@@ -17,7 +23,15 @@
 
                 <td class="role">
                     <span v-if="user.administrator">Administrator</span>
-                    <span v-if="!user.administrator">Nobody</span>
+                    <span v-if="!user.administrator">User</span>
+                </td>
+
+                <td class="last-seen">
+                    2 days ago
+                </td>
+
+                <td class="actions">
+                    <i class="fas fa-times"></i>
                 </td>
             </tr>
             </tbody>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="settings">
         <action-bar>
-            <action-bar-button text="Show me the statistics" icon="fas fa-chart-pie"></action-bar-button>
-            <action-bar-button text="Sign me out" icon="fas fa-sign-out-alt" @click="logout" :working="logoutInProgress"></action-bar-button>
             <action-bar-button text="Go back to the music" icon="fas fa-arrow-left" @click="goToBrowse"></action-bar-button>
+            <action-bar-button text="Sign me out" icon="fas fa-sign-out-alt" @click="logout" :working="logoutInProgress"></action-bar-button>
+            <action-bar-button text="Show me the statistics" icon="fas fa-chart-pie"></action-bar-button>
         </action-bar>
         <main-header text="Settings"></main-header>
 
