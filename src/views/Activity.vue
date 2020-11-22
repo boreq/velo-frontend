892d6fe48e9d271ab222d7a5c3bd49021daf4b44
boreq <boreq@0x46.net>
2020-11-22 02:16:27 +0100

    Add extra info to activity view

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -2,7 +2,36 @@
     <div class="activity">
         <main-header text="Activity"></main-header>
 
-        <div v-if="activity">
+        <div class="activity-content" v-if="activity">
+            <div class="avatar">
+                <a>
+                    <i class="far fa-user-circle"></i>
+                </a>
+            </div>
+
+            <div class="author">
+                <a>
+                    {{ activity.user.displayName }}
+                </a>
+            </div>
+
+            <div class="date">
+                <a>
+                    <time v-tooltip="'Activity started on ' + activity.timeStarted + ' and ended on ' +  activity.timeEnded + '.'">
+                        <timeago :datetime="activity.timeStarted"></timeago>
+                    </time>
+                </a>
+            </div>
+
+            <ul class="details">
+                <li v-tooltip="'Distance.'">
+                    12.1 km
+                </li>
+                <li v-tooltip="'Time moving.'">
+                    2.3 h
+                </li>
+            </ul>
+
             <route-map class="map" :route="activity.route"></route-map>
         </div>
     </div>
