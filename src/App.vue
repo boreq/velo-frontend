37519f8bbde31800cbbc497ccb65203987bb0090
boreq <boreq@0x46.net>
2020-11-22 02:34:03 +0100

    Full width header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="app">
-        <div class="container">
-            <div class="wrapper">
-                <div class="header">
-                    <site-header></site-header>
-                </div>
+        <div class="header">
+            <div class="container">
+                <div class="wrapper">
+                    <div class="header">
+                        <site-header></site-header>
+                    </div>
 
-                <div class="menu">
-                    <main-menu></main-menu>
+                    <div class="menu">
+                        <main-menu></main-menu>
+                    </div>
                 </div>
+            </div>
+        </div>
 
-                <div class="content">
+        <div class="content">
+            <div class="container">
+                <div class="wrapper">
                     <router-view/>
                 </div>
             </div>
