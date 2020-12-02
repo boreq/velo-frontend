44c0c1fa1670e7133e1a4f52f706173763acd9ca
boreq <boreq@0x46.net>
2020-12-02 20:08:59 +0100

    Remove unused components

--- src/components/ConversionStatus.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="conversion-status" v-tooltip="text" v-if="isConverting">
-        <spinner></spinner>
-    </div>
-</template>
-<script lang="ts" src="./ConversionStatus.ts"></script>
-<style scoped lang="scss" src="./ConversionStatus.scss"></style>
--- src/components/Player.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="player">
-        <audio controls ref="audio" @ended="onEnded" @error="onError" @play="onPlay" @pause="onPause"></audio>
-    </div>
-</template>
-<script lang="ts" src="./Player.ts"></script>
-<style scoped lang="scss" src="./Player.scss"></style>
