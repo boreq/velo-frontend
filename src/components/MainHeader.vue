947609438ca908cd635b2ca2a3dd62981a6cad7f
boreq <boreq@0x46.net>
2021-01-15 02:21:48 +0100

    Add placeholder actions to the activity view

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <h1 class="main-header">
         <div class="text">{{ text }}</div>
+        <div class="slot">
+            <slot></slot>
+        </div>
     </h1>
 </template>
 <script lang="ts" src="./MainHeader.ts"></script>
--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="activity">
-        <main-header text="Activity"></main-header>
+        <main-header text="Activity">
+            <ul class="header-actions">
+                <li>
+                    <a>Edit</a>
+                </li>
+                <li>
+                    <a>Delete</a>
+                </li>
+            </ul>
+        </main-header>
         <activity-header :activity="activity"></activity-header>
         <route-map class="map" :route="activity.route"></route-map>
     </div>
