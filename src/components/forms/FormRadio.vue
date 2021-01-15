cad97647c3b24280918fb29f241f3a1ca4127485
boreq <boreq@0x46.net>
2021-01-15 02:34:40 +0100

    Add a caret to show that the visibility dropdown is clickable

--- src/components/forms/FormRadio.vue
+++ src/components/forms/FormRadio.vue
@@ -2,7 +2,12 @@
     <div class="form-radio">
         <dropdown ref="dropdown">
             <template v-slot:trigger>
-                <i v-for="entry in values" :key="entry.value" v-if="value == entry.value" v-tooltip="entry.tooltip" class="trigger" :class="entry.icon"></i>
+                <div class="trigger">
+                    <i v-for="entry in values" :key="entry.value"
+                        v-if="value == entry.value" v-tooltip="entry.tooltip"
+                        :class="entry.icon" class="selection"></i>
+                    <i class="fas fa-caret-down call-to-action"></i>
+                </div>
             </template>
             <template v-slot:content>
                 <dropdown-element v-for="entry in values" :key="entry.value">
