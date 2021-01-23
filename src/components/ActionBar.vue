6f13a9a152c62cdbdaa7a99bf6baf548427dcbce
boreq <boreq@0x46.net>
2021-01-23 17:36:46 +0100

    Remove leftover components: action bar

--- src/components/ActionBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="action-bar">
-        <slot></slot>
-    </div>
-</template>
-<script lang="ts" src="./ActionBar.ts"></script>
-<style scoped lang="scss" src="./ActionBar.scss"></style>
